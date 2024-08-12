e355c8253e9c17044082e7c4279f443fa20a8a60
bytetriperTPU <bytetriper@TPU.google.com>
2024-08-12 08:29:29 +0000

    fixing xla bug in knn eval

--- eval/knn/dinov2/dinov2/eval/knn.py
+++ eval/knn/dinov2/dinov2/eval/knn.py
@@ -10,6 +10,8 @@ import logging
 import os
 import sys
 from typing import List, Optional
+
+import torch.distributed
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../../../')))
 print(sys.path)
 import torch
@@ -129,8 +131,21 @@ def get_args_parser(
         input_size=224,
     )
     return parser
+import threading
+def gather_with_timeout(tensor, output_tensor, dst, timeout):
+    def gather():
+        torch.distributed.gather(tensor, output_tensor, dst=dst)
+    
+    gather_thread = threading.Thread(target=gather)
+    gather_thread.start()
+    gather_thread.join(timeout)
 
-
+    if gather_thread.is_alive():
+        print("Gather operation timed out!")
+        # Optionally, you can kill the thread here, but it requires careful handling.
+        # Usually, you'd want to raise an exception or handle the timeout appropriately.
+        return False
+    return True
 class KnnModule(torch.nn.Module):
     """
     Gets knn of test features from all processes on a chunk of the train features
@@ -159,21 +174,23 @@ class KnnModule(torch.nn.Module):
     def _get_knn_sims_and_labels(self, similarity, train_labels):
         topk_sims, indices = similarity.topk(self.max_k, largest=True, sorted=True)
         neighbors_labels = torch.gather(train_labels, 1, indices)
+        print(f"topk_sims: {topk_sims.shape}, neighbors_labels: {neighbors_labels.shape}")
         return topk_sims, neighbors_labels
 
     def _similarity_for_rank(self, features_rank, source_rank):
         # Send the features from `source_rank` to all ranks
         broadcast_shape = torch.tensor(features_rank.shape).to(self.device)
         torch.distributed.broadcast(broadcast_shape, source_rank)
-
+        print(f"rank {self.global_rank} broadcast_shape: {broadcast_shape}")
         broadcasted = features_rank
         if self.global_rank != source_rank:
             broadcasted = torch.zeros(*broadcast_shape, dtype=features_rank.dtype, device=self.device)
         torch.distributed.broadcast(broadcasted, source_rank)
-
+        print(f"rank {self.global_rank} broadcasted: {broadcasted.shape}")
         # Compute the neighbors for `source_rank` among `train_features_rank_T`
         similarity_rank = torch.mm(broadcasted, self.train_features_rank_T)
         candidate_labels = self.candidates.expand(len(similarity_rank), -1)
+        print(f"rank {self.global_rank} similarity_rank: {similarity_rank.shape}, candidate_labels: {candidate_labels.shape}")
         return self._get_knn_sims_and_labels(similarity_rank, candidate_labels)
 
     def _gather_all_knn_for_rank(self, topk_sims, neighbors_labels, target_rank):
@@ -182,24 +199,30 @@ class KnnModule(torch.nn.Module):
         if self.global_rank == target_rank:
             topk_sims_rank = [torch.zeros_like(topk_sims) for _ in range(self.global_size)]
             retrieved_rank = [torch.zeros_like(neighbors_labels) for _ in range(self.global_size)]
-
-        torch.distributed.gather(topk_sims, topk_sims_rank, dst=target_rank)
-        torch.distributed.gather(neighbors_labels, retrieved_rank, dst=target_rank)
-
+        print(f"rank {self.global_rank} topk_sims: {topk_sims.shape}, neighbors_labels: {neighbors_labels.shape}")
+        #torch.distributed.gather(topk_sims, topk_sims_rank, dst=target_rank)
+        gather_with_timeout(topk_sims, topk_sims_rank, dst=target_rank, timeout=10)
+        print(f"gather1: rank {self.global_rank} topk_sims_rank: {topk_sims_rank[0].shape}")
+        #torch.distributed.gather(neighbors_labels, retrieved_rank, dst=target_rank)
+        gather_with_timeout(neighbors_labels, retrieved_rank, dst=target_rank, timeout=10)
+        print(f"gather2: rank {self.global_rank} topk_sims_rank: {topk_sims_rank[0].shape}, retrieved_rank: {retrieved_rank[0].shape}")
         if self.global_rank == target_rank:
             # Perform a second top-k on the k * global_size retrieved neighbors
             topk_sims_rank = torch.cat(topk_sims_rank, dim=1)
             retrieved_rank = torch.cat(retrieved_rank, dim=1)
             results = self._get_knn_sims_and_labels(topk_sims_rank, retrieved_rank)
+            print(f"master rank {self.global_rank} topk_sims_rank: {topk_sims_rank.shape}, retrieved_rank: {retrieved_rank.shape}")
             return results
         return None
 
     def compute_neighbors(self, features_rank):
         for rank in range(self.global_size):
             topk_sims, neighbors_labels = self._similarity_for_rank(features_rank, rank)
+            xm.master_print(f"compute neighbour: rank {self.global_rank} topk_sims: {topk_sims.shape}, neighbors_labels: {neighbors_labels.shape}")
             results = self._gather_all_knn_for_rank(topk_sims, neighbors_labels, rank)
             if results is not None:
                 topk_sims_rank, neighbors_labels_rank = results
+            xm.mark_step()
         return topk_sims_rank, neighbors_labels_rank
 
     def forward(self, features_rank):
@@ -207,14 +230,18 @@ class KnnModule(torch.nn.Module):
         Compute the results on all values of `self.nb_knn` neighbors from the full `self.max_k`
         """
         assert all(k <= self.max_k for k in self.nb_knn)
-
+        xm.master_print(f"features_rank: {features_rank.shape}")
         topk_sims, neighbors_labels = self.compute_neighbors(features_rank)
+        xm.mark_step()
+        xm.master_print(f"forward1: topk_sims: {topk_sims.shape}, neighbors_labels: {neighbors_labels.shape}")
         batch_size = neighbors_labels.shape[0]
         topk_sims_transform = softmax(topk_sims / self.T, 1)
         matmul = torch.mul(
             one_hot(neighbors_labels, num_classes=self.num_classes),
             topk_sims_transform.view(batch_size, -1, 1),
         )
+        xm.mark_step()
+        xm.master_print(f"matmul: {matmul.shape}")
         probas_for_k = {k: torch.sum(matmul[:, :k, :], 1) for k in self.nb_knn}
         return probas_for_k
 
@@ -274,7 +301,11 @@ def create_class_indices_mapping(labels):
 
 class ModuleDictWithForward(torch.nn.ModuleDict):
     def forward(self, *args, **kwargs):
-        return {k: module(*args, **kwargs) for k, module in self._modules.items()}
+        return_dict = {}
+        for k, module in self.items():
+            return_dict[k] = module(*args, **kwargs)
+            xm.mark_step()
+        return return_dict
 
 
 def eval_knn(
