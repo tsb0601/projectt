a659e47d17ab14edb8205f8aa6e2d074dfac204b
bytetriperTPU <bytetriper@TPU.google.com>
2024-09-04 12:22:17 +0000

    add mae r

--- train_stage1.sh
+++ train_stage1.sh
@@ -34,8 +34,8 @@ else
     echo "wanb_id: $wandb_id"
     export WANDB_ID=$wandb_id
 fi
-#export WANDB_DIR=$SAVE_DIR
-#export WANDB_PROJECT=$EXP_NAME
+export WANDB_DIR=$SAVE_DIR
+export WANDB_PROJECT=$EXP_NAME
 env | grep WANDB
 export XLA_DISABLE_FUNCTIONALIZATION=1
 python main_stage1.py \
