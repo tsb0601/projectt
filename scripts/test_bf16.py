b48acccfab3e23671566a01855462e761fc4e816
bytetriperTPU <bytetriper@TPU.google.com>
2024-09-12 21:57:47 +0000

    add interesting bf16 test

--- scripts/test_bf16.py
+++ scripts/test_bf16.py
@@ -14,7 +14,7 @@ preturbed_bf16_tensor = torch.tensor([preturbed_bf16_value], dtype=torch.bfloat1
 print(f"minimum_bf16_tensor: {minimum_bf16_tensor.item()}, preturbed_bf16_tensor: {preturbed_bf16_tensor.item()}")
 
 above1_bf16_value = minimum_bf16_value + 1
-above1_preturbed_value = preturbed_bf16_value + 0.0001
+above1_preturbed_value = preturbed_bf16_value + 1
 above1_bf16_tensor = torch.tensor([above1_bf16_value], dtype=torch.bfloat16, device=device)
 above1_preturbed_tensor = torch.tensor([above1_preturbed_value], dtype=torch.bfloat16, device=device)
 print(f"above1_bf16_tensor: {above1_bf16_tensor.item()}, above1_precise_tensor: {above1_preturbed_tensor.item()}")
