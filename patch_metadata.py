fb40f5002a2e9db597d5e5fb1608b2bf6e293658
bytetriperTPU <bytetriper@TPU.google.com>
2024-10-20 21:54:04 +0000

    add bs4096 ema XL/2 klvea DiT

--- patch_metadata.py
+++ patch_metadata.py
@@ -15,10 +15,6 @@ def _get_metadata_reload(metadata_name, env_metadata, network_endpoint_metadata)
         raise ValueError(f"Unknown metadata '{metadata_name}'.")
     
 def patch_metadata():
-    env_metadata = load_metadata("/home/bytetriper/tpu-env.txt")
-    network_endpoint_metadata = load_metadata("/home/bytetriper/worker-network-endpoints.txt")
-    print(type(env_metadata))
-    print("--------------------")
-    print(type(network_endpoint_metadata))  
+    env_metadata = load_metadata("/home/bytetriper/tpu-env.txt") # modify this to your path
+    network_endpoint_metadata = load_metadata("/home/bytetriper/worker-network-endpoints.txt")  # modify this to your path
     tpu._get_metadata = partial(_get_metadata_reload, env_metadata=env_metadata, network_endpoint_metadata=network_endpoint_metadata)
-    print("--------------------")
