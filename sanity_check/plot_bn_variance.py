4d4c25aacacb18359f34273d4e8053b68f900c0c
bytetriperTPU <bytetriper@TPU.google.com>
2024-11-26 12:24:28 +0000

    add MAE gan

--- sanity_check/plot_bn_variance.py
+++ sanity_check/plot_bn_variance.py
@@ -6,9 +6,10 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 path = sys.argv[1]
 model = torch.load(path)
 print(model)
-running_mean = model.bn.running_mean
-running_var = model.bn.running_var
+C = model.bn.running_var.shape[0]
+running_mean = model.bn.running_mean.view(C, -1)
+running_var = model.bn.running_var.view(C, -1)
 # print the mean of var of running_mean
-print(running_mean.var(dim=0).mean(), running_mean.abs().mean(),running_mean.device)
+print(running_mean.var(dim=-1).mean(), running_mean.abs().mean(),running_mean.device)
 # print the var of var of running_var
-print(running_var.var(dim=0).mean(), running_var.mean())
+print(running_var.var(dim=-1).mean(), running_var.mean())
