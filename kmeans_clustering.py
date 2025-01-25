124f5477e003189af19205dfeb7dffcf4da518b1
tsb0601 <tsb@berkeley.edu>
2025-01-25 08:18:14 +0000

    sdasda

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -185,7 +185,7 @@ class SimpleTPUKMeans:
             new_centroids[mask] /= counts[mask].unsqueeze(1)
             self.centroids = new_centroids
             
-            if (iteration + 1) % 1 == 0 and self.checkpoint_dir: self._save_checkpoint(iteration)
+            if (iteration + 1) % 10 == 0 and self.checkpoint_dir: self._save_checkpoint(iteration)
             
             diff = self.centroids - old_centroids
             centroid_shift = torch.norm(diff, p=2, dim=1).mean().item()
